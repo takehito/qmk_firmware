360c907b9c4b59a4cf74f7869f33751cf782cbb5
takehito <fukuda_kento@fukehito.com>
2021-01-27 00:31:47 +0900

    change oled output

--- keyboards/crkbd/keymaps/mymap/keymap.c
+++ keyboards/crkbd/keymaps/mymap/keymap.c
@@ -115,7 +115,7 @@ void oled_render_layer_state(void) {
     oled_write_P(PSTR("Layer: "), false);
     switch (layer_state) {
         case L_BASE:
-            oled_write_ln_P(PSTR("Default"), false);
+            oled_write_ln_P(PSTR("Qwerty:"), false);
             break;
         case L_LOWER:
             oled_write_ln_P(PSTR("Lower"), false);
